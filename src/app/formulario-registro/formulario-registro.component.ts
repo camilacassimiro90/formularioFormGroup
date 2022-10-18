9563d385b173886a3dfbde46c67514be9721ec1d
Camila Cassimiro <camilacassimiro@gmail.com>
2022-10-18 20:41:19 -0300

    [M3S07] EX 5 - FormGroup e FormControl

--- src/app/formulario-registro/formulario-registro.component.ts
+++ src/app/formulario-registro/formulario-registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-registro',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario-registro.component.css']
 })
 export class FormularioRegistroComponent implements OnInit {
+  title = "Formulário de Registro";
+
+  formRegistro = new FormGroup({
+    nome: new FormControl(),
+    cargaHoraria: new FormControl(),
+    escola: new FormControl(),
+    email: new FormControl(),
+    professor: new FormControl(),
+    materia: new FormControl(),
+
+  });
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
-
 }
+
